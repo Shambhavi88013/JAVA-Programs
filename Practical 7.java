a23090cc07341dec41f4911bbd1d322fb64b1631
Shambhavi88013 <81892389+Shambhavi88013@users.noreply.github.com>
2021-06-12 21:16:16 +0530

    Add files via upload

---
+++ Practical 7.java
@@ -0,0 +1,24 @@
+package Practicals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileHandling2 {
+    public static void main(String[] args) throws IOException {
+
+        String line;
+        String content = "";
+
+        File myFile = new File("test.txt");
+        Scanner sc = new Scanner(myFile);
+
+        while(sc.hasNextLine()){
+            line = sc.nextLine();
+            if (line.startsWith("//")){
+                content = content + line + "\n";
+            }
+        }
+        System.out.println(content);
+    }
+}
